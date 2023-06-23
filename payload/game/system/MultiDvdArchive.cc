7793a25bda688e720c69b77d162c610f719d3ea6
Pablo Stebler <pablo@stebler.xyz>
2023-06-23 22:49:30 +0200

    system: Use the language setting in MultiDvdArchive

--- payload/game/system/MultiDvdArchive.cc
+++ payload/game/system/MultiDvdArchive.cc
@@ -2,53 +2,15 @@
 
 #include "game/system/ResourceManager.hh"
 
+#include <sp/settings/GlobalSettings.hh>
+#include <sp/settings/Language.hh>
+
 extern "C" {
 #include <revolution.h>
 #include <stdio.h>
 }
 
 namespace System {
-
-static const char *getBaseLanguageCode() {
-    switch (REGION) {
-    case REGION_P:
-        return "E";
-    case REGION_E:
-        return "U";
-    case REGION_J:
-        return "J";
-    case REGION_K:
-        return "K";
-    default:
-        assert(false);
-    }
-}
-
-static const char *getLanguageCode() {
-    switch (SCGetLanguage()) {
-    case SC_LANG_JAPANESE:
-        return "J";
-    case SC_LANG_ENGLISH:
-        return REGION == REGION_E ? "U" : "E";
-    case SC_LANG_SIMP_CHINESE:
-    case SC_LANG_TRAD_CHINESE:
-        return "E";
-    case SC_LANG_GERMAN:
-        return "G";
-    case SC_LANG_FRENCH:
-        return REGION == REGION_E ? "Q" : "F";
-    case SC_LANG_SPANISH:
-        return REGION == REGION_E ? "M" : "S";
-    case SC_LANG_ITALIAN:
-        return "I";
-    case SC_LANG_DUTCH:
-        return "N";
-    case SC_LANG_KOREAN:
-        return "K";
-    default:
-        assert(false);
-    }
-}
 
 void MultiDvdArchive::setMission(u32 missionId) {
     m_formats[1] = Format::Single;
@@ -101,16 +63,17 @@ RaceMultiDvdArchive::~RaceMultiDvdArchive() = default;
 void RaceMultiDvdArchive::init() {
     snprintf(m_names[0], 0x80, ".szs");
 
-    if (SCGetLanguage() == SC_LANG_KOREAN) {
+    if (SP::GlobalSettings::Get<SP::GlobalSettings::Setting::Language>() ==
+            SP::GlobalSettings::Language::Korean) {
         snprintf(m_names[1], 0x80, "_J.szs");
     } else {
-        snprintf(m_names[1], 0x80, "_%s.szs", getLanguageCode());
+        snprintf(m_names[1], 0x80, "_%s.szs", SP::Language::GetCode());
     }
 
     // New archives
     snprintf(m_names[2], 0x80, "SP.szs");
     snprintf(m_names[3], 0x80, "_Dif.szs");
-    snprintf(m_names[4], 0x80, "_%s_Dif.szs", getLanguageCode());
+    snprintf(m_names[4], 0x80, "_%s_Dif.szs", SP::Language::GetCode());
 
     for (size_t i = 0; i < 5; i++) {
         m_formats[i] = Format::Double;
@@ -130,11 +93,11 @@ void MenuMultiDvdArchive::init() {
         snprintf(m_names[0], 0x80, ".szs");
     }
 
-    snprintf(m_names[1], 0x80, "_%s.szs", getBaseLanguageCode());
+    snprintf(m_names[1], 0x80, "_%s.szs", SP::Language::GetBaseCode());
     snprintf(m_names[2], 0x80, "SP.szs");
-    snprintf(m_names[3], 0x80, "SP_%s.szs", getLanguageCode());
+    snprintf(m_names[3], 0x80, "SP_%s.szs", SP::Language::GetCode());
     snprintf(m_names[4], 0x80, "_Dif.szs");
-    snprintf(m_names[5], 0x80, "_%s_Dif.szs", getLanguageCode());
+    snprintf(m_names[5], 0x80, "_%s_Dif.szs", SP::Language::GetCode());
 
     for (size_t i = 0; i < 6; i++) {
         m_formats[i] = Format::Double;
